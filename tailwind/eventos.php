efb2f6106980b4b87efa1065c97916f5f5aee0c7
psoares-iscac <psoares@iscac.pt>
2025-11-06 18:23:29 +0000

    Update eventos.php

--- tailwind/eventos.php
+++ tailwind/eventos.php
@@ -1,3 +1,13 @@
+<?php 
+$user = 'web1';
+$pass = 'web1';
+try {
+    $dbh = new PDO('mysql:host=localhost;dbname=web1', $user, $pass);
+} catch (PDOException $e) {
+    // attempt to retry the connection after some timeout for example
+    #echo 'erro';
+}
+?>
 <!DOCTYPE html>
 <html lang="pt-pt">
 <head>
@@ -19,45 +29,39 @@
 
     <p class="p-5 font-light text-5xl text-center">Galeria de imagens</p>
 
+    <?php 
+    $sql = 'SELECT * FROM eventos';
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute();
+    #echo $stmt->rowCount();
+    ?>
+
     <div class="mx-auto max-w-7xl">
         
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
 
+        <?php 
+        while($evento = $stmt->fetchObject()){
+            $idEvento   = $evento->id;
+            $nome       = $evento->nome;
+            $imagem     = $evento->imagem;
+        ?>
             <div class="">
-                <img class="w-full h-auto" src="imgs/imagem3_min.jpg" alt="Evento 3">
+                <img class="w-full h-auto" src="imgs/<?= $imagem ?>" alt="Evento <?= $idEvento ?>">
                 <div class="my-1 font-semibold">29.11</div>
-                <div class="font-light mb-4">Festa de fim de semestre</div>
-                <a href="evento.php?evento=3" class="inline-flex items-center gap-2 p-2 border border-[rgb(56,142,60)] text-[rgb(76,175,80)] hover:bg-[rgb(56,142,60)] hover:text-white">
+                <div class="font-light mb-4"><?= $nome ?></div>
+                <a href="evento.php?evento=<?= $idEvento ?>" class="inline-flex items-center gap-2 p-2 border border-[rgb(56,142,60)] text-[rgb(76,175,80)] hover:bg-[rgb(56,142,60)] hover:text-white">
                     Ver mais <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-move-right-icon lucide-move-right"><path d="M18 8L22 12L18 16"/><path d="M2 12H22"/></svg>
                 </a>
             </div>
 
-            <div class="">
-                <img class="w-full h-auto" src="imgs/imagem1_min.jpg" alt="Evento 1">
-                <div class="my-1 font-semibold">31.12</div>
-                <div class="font-light mb-4">Bem vindo 2026!</div>
-                <a href="evento.html?evento=1" class="inline-flex items-center gap-2 p-2 border border-[rgb(56,142,60)] text-[rgb(76,175,80)] hover:bg-[rgb(56,142,60)] hover:text-white">
-                    Ver mais <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-move-right-icon lucide-move-right"><path d="M18 8L22 12L18 16"/><path d="M2 12H22"/></svg>
-                </a>
-            </div>
+        <?php 
+        } # do while de repeticao de eventos
+        ?>
 
-            <div class="">
-                <img class="w-full h-auto" src="imgs/imagem2_min.jpg" alt="Evento 2">
-                <div class="my-1 font-semibold">05.04</div>
-                <div class="font-light mb-4">Páscoa Festiva</div>
-                <a href="evento.html?evento=2" class="inline-flex items-center gap-2 p-2 border border-[rgb(56,142,60)] text-[rgb(76,175,80)] hover:bg-[rgb(56,142,60)] hover:text-white">
-                    Ver mais <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-move-right-icon lucide-move-right"><path d="M18 8L22 12L18 16"/><path d="M2 12H22"/></svg>
-                </a>
-            </div>
+            
 
-            <div class="">
-                <img class="w-full h-auto" src="imgs/imagem4_min.jpg" alt="Evento 4">
-                <div class="my-1 font-semibold">fevereiro</div>
-                <div class="font-light mb-4">Festividades várias</div>
-                <a href="evento.html?evento=4" class="inline-flex items-center gap-2 p-2 border border-[rgb(56,142,60)] text-[rgb(76,175,80)] hover:bg-[rgb(56,142,60)] hover:text-white">
-                    Ver mais <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-move-right-icon lucide-move-right"><path d="M18 8L22 12L18 16"/><path d="M2 12H22"/></svg>
-                </a>
-            </div>
+           
             
 
         </div>
