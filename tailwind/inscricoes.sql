44bff10e2aad02c973833924e4e4a92ea4eb2369
Paulo Soares <psoares@iscac.pt>
2025-11-20 11:58:05 +0000

    13

--- tailwind/inscricoes.sql
+++ tailwind/inscricoes.sql
@@ -17,6 +17,7 @@
 -- A despejar estrutura para tabela web1.inscricoes
 CREATE TABLE IF NOT EXISTS `inscricoes` (
   `id` int NOT NULL AUTO_INCREMENT,
+  `eventoId` int NOT NULL,
   `email` varchar(128) NOT NULL,
   `nome` varchar(128) NOT NULL,
   `telefone` varchar(128) NOT NULL,
@@ -25,14 +26,14 @@ CREATE TABLE IF NOT EXISTS `inscricoes` (
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- A despejar dados para tabela web1.inscricoes: ~7 rows (aproximadamente)
-INSERT INTO `inscricoes` (`id`, `email`, `nome`, `telefone`, `socio`) VALUES
-	(1, 'joao.silva@exemplo.pt	', 'João Silva	', '912345678', 0),
-	(2, 'mariana.costa@email.com	', 'Mariana Costa	', '934567890', 0),
-	(3, 'pedro.almeida@empresa.pt	', 'Pedro Almeida	', '965432101', 0),
-	(4, 'ana.ribeiro@servico.com	', 'Ana Ribeiro	', '911223344', 0),
-	(5, 'carlos.santos@exemplo.pt	', 'Carlos Santos	', '933445566', 1),
-	(6, 'sofia.ferreira@email.com	', 'Sofia Ferreira	', '966778899', 0),
-	(7, 'beatriz.pereira@servico.com	', 'Beatriz Pereira	', '938765432', 0);
+INSERT INTO `inscricoes` (`id`, `eventoId`, `email`, `nome`, `telefone`, `socio`) VALUES
+	(1, 1, 'joao.silva@exemplo.pt	', 'João Silva	', '912345678', 0),
+	(2, 1, 'mariana.costa@email.com	', 'Mariana Costa	', '934567890', 0),
+	(3, 1, 'pedro.almeida@empresa.pt	', 'Pedro Almeida	', '965432101', 0),
+	(4, 1, 'ana.ribeiro@servico.com	', 'Ana Ribeiro	', '911223344', 0),
+	(5, 1, 'carlos.santos@exemplo.pt	', 'Carlos Santos	', '933445566', 1),
+	(6, 1, 'sofia.ferreira@email.com	', 'Sofia Ferreira	', '966778899', 0),
+	(7, 1, 'beatriz.pereira@servico.com	', 'Beatriz Pereira	', '938765432', 0);
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
