e5f522aece81b3598bf63ae020260ab043f671a4
psoares-iscac <psoares@iscac.pt>
2025-12-04 17:35:51 +0000

    24

--- tailwind/admin/includes/connection.php
+++ tailwind/admin/includes/connection.php
@@ -1,9 +1,9 @@
 <?php
-$user = 'web2';
-$pass = 'web2';
+$user = 'web1';
+$pass = 'web1';
 
 try {
-    $dbh = new PDO('mysql:host=localhost;dbname=web2', $user, $pass);
+    $dbh = new PDO('mysql:host=localhost;dbname=web1', $user, $pass);
 } catch (PDOException $e) {
     // attempt to retry the connection after some timeout for example
     echo 'Erro';
